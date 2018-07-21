fcf7464922f8d3500338b1356d2f84b2ebb2177b
Paulina Dabrowska <14799673+Trufs@users.noreply.github.com>
2018-07-21 23:02:41 +0200

    Make Bookshelf changer able to update its state

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 class BookShelfChanger extends Component{
+  state = {
+    value: "none"
+  }
+
+  handleChange = (event) => {
+    this.setState({value: event.target.value});
+  }
+
 	render() {
     	return (
 	        <div className="book-shelf-changer">
-	          <select>
+	          <select value={this.state.value} onChange={this.handleChange}>
 	            <option value="move" disabled>Move to...</option>
 	            <option value="currentlyReading">Currently Reading</option>
 	            <option value="wantToRead">Want to Read</option>
