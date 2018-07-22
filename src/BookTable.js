475b95b31a2174a121ae3f31c6ea4df3cec78ff1
Paulina Dabrowska <14799673+Trufs@users.noreply.github.com>
2018-07-22 14:09:57 +0200

    Fetch books when query changes

--- src/BookTable.js
+++ src/BookTable.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import * as BooksAPI from './BooksAPI';
 
 class BookTable extends Component{
-	render() {
+
+  state = {
+    books: []
+  }
+
+   componentDidMount() {
+    console.log(this.props.query);
+    BooksAPI.search(this.props.query).then((books) => {
+      this.setState({ books })
+    })
+  }
+
+  render() {
+
+    // let showingBooks;
+    // if (query) {
+    //    const match = new RegExp(escapeRegExp(query), 'i');
+    //    showingBooks = books.filter((book) => match.test(book.title || book.authors));
+    // } else {
+    //   showingBooks = books;
+    // }
+
+    // showingBooks.sort(sortBy('name'));
+
     	return (
 
-
-              <ol className="books-grid"></ol>
-
-
+              <ol className="books-grid">
+              </ol>
     	)
     }
 }
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import * as BooksAPI from './BooksAPI';
 import SearchBar from './SearchBar';
 import BookTable from './BookTable';
 import escapeRegExp from 'escape-string-regexp';
@@ -10,13 +11,19 @@ class SearchPage extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      query: ''
+      query: '',
+      books: []
     }
     this.updateQuery = this.updateQuery.bind(this);
   }
 
   updateQuery = (query) => {
+    console.log(query);
     this.setState({query: query.trim() });
+    BooksAPI.search(this.state.query).then((books) => {
+      this.setState({ books: books })
+    })
+    console.log(this.state.books);
   }
 
 	render() {
