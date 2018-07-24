902a77d71ed7b12793a51944a0e7cca1f31359d3
Paulina Dabrowska <14799673+Trufs@users.noreply.github.com>
2018-07-24 21:00:13 +0200

    Handle lack of cover in book data

--- src/Book.js
+++ src/Book.js
@@ -29,8 +29,9 @@ class Book extends Component{
 
           <div className="book-top">
 
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${imageLinks.thumbnail}` }}>
-            </div>
+          {
+            imageLinks && <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${imageLinks.thumbnail}` }}></div>
+          }
               <div className="book-shelf-changer">
                 <select value={this.state.value} onChange={this.shelfChange}>
                   <option value="move" disabled>Move to...</option>
